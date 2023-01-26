937584739c8959c5178f78694a856f1bbf71ed0f
MORONGWA <104618822+moronngwa@users.noreply.github.com>
2023-01-26 10:25:15 +0200

    Add project files.

---
+++ Morongwapolymorphism/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morongwapolymorphism
+{
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+
+    }
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says:wee wee");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says:bow woo");
+        }
+    }
+
+    class Goat : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The goat says:mee mee");
+        }
+    }
+}
+
---
+++ Morongwapolymorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using Morongwapolymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Pig pig1 = new Pig();
+        Dog dog1 = new Dog();
+        Goat goat1 = new Goat();
+
+        pig1.animalSound();
+        dog1.animalSound();
+        goat1.animalSound();
+    }
+}
+
